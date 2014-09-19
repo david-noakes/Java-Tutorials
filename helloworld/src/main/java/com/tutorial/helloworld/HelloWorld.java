5b58d2c3c833da076e993e6a8c46c4b3af71e05f
david <david_noakes@dialog.com.au>
2014-09-19 17:27:50 +1000

    move files into correct directory

--- helloworld/src/main/java/com/tutorial/helloworld/HelloWorld.java
+++ helloworld/src/main/java/com/tutorial/helloworld/HelloWorld.java
@@ -1,9 +1,17 @@
 package com.tutorial.helloworld;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 
 @ManagedBean(name = "helloWorld", eager = true)
+@RequestScoped
 public class HelloWorld {
+	   @ManagedProperty(value="#{message}")
+	   private Message messageBean;
+
+	   private String message;
+
    public HelloWorld() {
       System.out.println("HelloWorld started!");
    }
