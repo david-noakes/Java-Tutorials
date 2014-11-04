ded251a67705b9a98e18d8e86f862b83a076add1
david <david.noakes@fuguesoftware.com>
2014-11-04 15:00:26 +1000

    finish the JSF Prime Faces tutorial

---
+++ boreas/src/main/java/com/tutorial/boreas/GrowlBean.java
@@ -0,0 +1,26 @@
+package com.tutorial.boreas;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class GrowlBean {
+    public String makeMessages() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        FacesMessage message1 = 
+            new FacesMessage("Something happened");
+        // Default severity is INFO
+        context.addMessage(null, message1);
+        FacesMessage message2 = 
+            new FacesMessage("You were naughty");
+        message2.setSeverity(FacesMessage.SEVERITY_ERROR);
+        context.addMessage(null, message2);
+        return(null);
+    }
+
+    public GrowlBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ boreas/src/main/java/com/tutorial/boreas/SmartThemeChoices.java
@@ -0,0 +1,27 @@
+package com.tutorial.boreas;
+
+import java.util.*;
+
+import javax.faces.bean.*;
+
+@ManagedBean
+public class SmartThemeChoices {
+
+    /** Returns a List of all possible themes, with the current theme at the front. */
+    
+    public List<String> getThemes() {
+      List<String> themes = new LinkedList<String>();
+      for(String theme: ThemeChoices.POSSIBLE_THEMES) {
+        themes.add(theme);
+      }
+      String currentTheme = ThemeUtils.currentTheme();
+      themes.remove(currentTheme);
+      themes.add(0, currentTheme);
+      return(themes);
+    }
+
+    public SmartThemeChoices() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
