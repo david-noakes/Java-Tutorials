5cf29f469dba45abf367c456c7da49b9055b83e2
david <david.noakes@gmail.com>
2018-07-20 23:58:20 +1000

    TreeTraversal

---
+++ Trees/src/org/erehwon/shadowlands/trees/TreeBottom.java
@@ -0,0 +1,537 @@
+package org.erehwon.shadowlands.trees;
+
+ 
+
+import java.util.ArrayList;
+
+import java.util.HashMap;
+
+import java.util.List;
+
+ 
+
+// basic tree node
+
+class Node
+
+{
+
+    int key;
+
+    Node left, right;
+
+    public Node(int item)
+
+    {
+
+        key = item;
+
+        left = right = null;
+
+    }
+
+    public Node() {
+
+      key = -1;
+
+      left = null;
+
+      right = null;
+
+    }
+
+}
+
+ 
+
+// A Java program to introduce Binary Tree
+
+ 
+
+class BinaryTree
+
+{
+
+ 
+
+    // Root of Binary Tree
+
+    Node root;
+
+    String[] source;
+
+ 
+
+    int maxDepth;
+
+ 
+
+    int srcIdx;
+
+ 
+
+    // Constructors
+
+    BinaryTree(int key)
+
+    {
+
+        root = new Node(key);
+
+        source = null;
+
+        maxDepth = 1;
+
+    }
+
+ 
+
+    BinaryTree()
+
+    {
+
+        root = null;
+
+        source = null;
+
+        maxDepth = 0;
+
+    }
+
+ 
+
+}
+
+ 
+
+ 
+
+public class TreeBottom {
+
+       String VALUE = "value";
+
+       String LEFT_BRANCH = "leftBranch";
+
+       String RIGHT_BRANCH = "rightBranch";
+
+       String DEPTH = "depth";
+
+ 
+
+       List<HashMap> bottomNodes = new ArrayList<HashMap>();
+
+ 
+
+       int currentDepth = 0;
+
+ 
+
+       BinaryTree forest = new BinaryTree();
+
+ 
+
+       int[] treeBottom(String tree) {
+
+ 
+
+         tree = tree.substring(1);
+
+        
+
+         String[] parts = tree.split("\\(");
+
+         for (int i = 0; i < parts.length; i++) {
+
+                parts[i] = parts[i].trim();
+
+         }
+
+ 
+
+         forest.source = parts;
+
+         forest.srcIdx = 0;
+
+         HashMap nMap = new HashMap();
+
+         int val = Integer.parseInt(parts[0].trim());
+
+         forest.srcIdx = 1;
+
+         currentDepth = 1;
+
+         nMap.put(VALUE, parts[0].trim());
+
+         nMap.put(DEPTH, currentDepth);
+
+         Node node = new Node(val);
+
+         forest.root = node;
+
+ 
+
+         // remove the (nn and final)
+
+         //tree = tree.substring(1+parts[1].length(), tree.length() - 1)
+
+ 
+
+//       System.out.println("About to call traverseLeft");
+
+ 
+
+         if (!parts[forest.srcIdx].equals(")")  ) {
+
+            HashMap x = traverseLeft(nMap, tree, 1, forest, node);
+
+            nMap.put(LEFT_BRANCH, x);
+
+         } else {
+
+                forest.srcIdx++;
+
+         }
+
+ 
+
+//       System.out.println("About to call traverseRight idx="+forest.srcIdx);
+
+         while ((forest.srcIdx < forest.source.length -1)) {
+
+                if (!parts[forest.srcIdx].startsWith(")")  ) {
+
+                   HashMap y = traverseRight(nMap, tree, 1, forest, node);
+
+                   nMap.put(RIGHT_BRANCH, y);
+
+                } else
+
+                if (parts[forest.srcIdx].startsWith("))") && parts[forest.srcIdx].length() > 2 ) {
+
+                    parts[forest.srcIdx] = parts[forest.srcIdx].substring(2);
+
+                } else {
+
+                    forest.srcIdx++;
+
+                }
+
+         }
+
+ 
+
+//       System.out.println("About to call Add To Tree");
+
+ 
+
+      
+
+         assert((forest.source.length - 1) == forest.srcIdx);
+
+         addToTree(nMap);
+
+         int[] result = new int[bottomNodes.size()];
+
+         for (int i = 0; i < bottomNodes.size(); i++) {
+
+                result[i] = (int) Integer.parseInt( (String)(bottomNodes.get(i).get(VALUE)));
+
+         }
+
+         return result;
+
+ 
+
+        
+
+ 
+
+       }
+
+ 
+
+      
+
+ 
+
+       // (n (left) (right))
+
+ 
+
+       HashMap traverseLeft(HashMap map, String tree, int depth, BinaryTree forest, Node currNode) {
+
+ 
+
+         String[] parts = forest.source;
+
+ 
+
+         HashMap nMap = new HashMap();
+
+ 
+
+         if (parts[forest.srcIdx].startsWith(")") ) {
+
+//                System.out.println("traverseLeft early exit idx=" + forest.srcIdx);
+
+        forest.srcIdx++;
+
+           return nMap;
+
+ 
+
+         }
+
+ 
+
+         int currentDepth = depth + 1;
+
+         nMap.put(VALUE, parts[forest.srcIdx].trim());
+
+         nMap.put(DEPTH, currentDepth);
+
+ 
+
+         Node node = new Node( Integer.parseInt(parts[forest.srcIdx].trim()));
+
+         currNode.left = node;
+
+      
+
+         forest.srcIdx++;
+
+         if (forest.srcIdx < parts.length) {
+
+                if (!parts[forest.srcIdx].equals(")")  ) {
+
+                   HashMap x = traverseLeft(nMap, tree, currentDepth, forest, node);
+
+                   nMap.put(LEFT_BRANCH, x);
+
+                } else {
+
+                       forest.srcIdx++;
+
+                }
+
+         }
+
+        
+
+         if (forest.srcIdx < parts.length && parts[forest.srcIdx].equals(")") ) {
+
+          forest.srcIdx++;
+
+         }
+
+        
+
+         if (forest.srcIdx < parts.length) {
+
+                if (!parts[forest.srcIdx].startsWith(")")  ) {
+
+                   HashMap y = traverseRight(nMap, tree, currentDepth, forest, node);
+
+                   nMap.put(RIGHT_BRANCH, y);
+
+                } else {
+
+                  if (parts[forest.srcIdx].length() > 2) {
+
+                    parts[forest.srcIdx] = parts[forest.srcIdx].substring(2);
+
+                  } else {
+
+                    forest.srcIdx++;
+
+                  }
+
+                }
+
+         }
+
+ 
+
+         if (forest.srcIdx < parts.length && parts[forest.srcIdx].equals(")") ) {
+
+          forest.srcIdx++;
+
+         }
+
+        
+
+ 
+
+         addToTree(nMap);
+
+         map.put(LEFT_BRANCH, nMap);
+
+         return map;
+
+       }
+
+ 
+
+      
+
+ 
+
+       HashMap traverseRight(HashMap map, String tree, int depth, BinaryTree forest, Node currNode) {
+
+ 
+
+         String[] parts = forest.source;
+
+         HashMap nMap = new HashMap();
+
+ 
+
+         if (forest.srcIdx < parts.length && parts[forest.srcIdx].startsWith(")")) {
+
+                if (parts[forest.srcIdx].startsWith("))") ) {
+
+                  if (parts[forest.srcIdx].length() > 2) {
+
+                    parts[forest.srcIdx] = parts[forest.srcIdx].substring(2);
+
+                  } else {
+
+                    forest.srcIdx++;
+
+                  }
+
+                } else {
+
+                       forest.srcIdx++;
+
+                }
+
+           return nMap;
+
+         }
+
+ 
+
+         if (forest.srcIdx < parts.length) {
+
+                if (parts[forest.srcIdx].trim().length() == 0) {
+
+      
+
+       //         System.out.println("traverseRight short exit idx=" + forest.srcIdx + ": " +      parts[forest.srcIdx]);
+
+                  forest.srcIdx++;
+
+                  return nMap;
+
+                }
+
+         }
+
+ 
+
+         int currentDepth = depth + 1;
+
+         nMap.put(VALUE, parts[forest.srcIdx].trim());
+
+         nMap.put(DEPTH, currentDepth);
+
+ 
+
+         Node node = new Node( Integer.parseInt(parts[forest.srcIdx]));
+
+         currNode.right = node;
+
+         forest.srcIdx++;
+
+ 
+
+         if (forest.srcIdx < parts.length) {
+
+                if (!parts[forest.srcIdx].equals(")")  ) {
+
+                   HashMap x = traverseLeft(nMap, tree, currentDepth, forest, node);
+
+                   nMap.put(LEFT_BRANCH, x);
+
+                } else {
+
+                       forest.srcIdx++;
+
+                }     
+
+         }
+
+      
+
+         if (forest.srcIdx < parts.length) {
+
+                if (!parts[forest.srcIdx].startsWith(")")  ) {
+
+                  HashMap y = traverseRight(nMap, tree, currentDepth, forest, node);
+
+                  nMap.put(RIGHT_BRANCH, y);
+
+                } else {
+
+                  if (parts[forest.srcIdx].length() > 2) {
+
+                    parts[forest.srcIdx] = parts[forest.srcIdx].substring(2);
+
+                  } else {
+
+                    forest.srcIdx++;
+
+                  }
+
+                }          
+
+         }
+
+         addToTree(nMap);
+
+         map.put(RIGHT_BRANCH, nMap);
+
+         return map;
+
+ 
+
+       }
+
+ 
+
+      
+
+ 
+
+       void addToTree(HashMap x){
+
+ 
+
+//        System.out.println("In addToTree " + x);
+
+          int iDepth = (Integer)x.get(DEPTH);
+
+          if (iDepth > currentDepth) {
+
+              bottomNodes = new ArrayList<HashMap>();
+
+              currentDepth = iDepth;
+
+              bottomNodes.add(x);
+
+           } else
+
+           if (iDepth == currentDepth) {
+
+              bottomNodes.add(x);
+
+           }
+
+       }
+
+      
+
+}
+
+ 
---
+++ Trees/src/org/erehwon/shadowlands/trees/TreeBottomDemo.java
@@ -0,0 +1,45 @@
+package org.erehwon.shadowlands.trees;
+
+import java.util.Arrays;
+
+public class TreeBottomDemo {
+
+   //Tree data:
+	static String Test1 = "(1 () ())";	
+	static String Test2 = "(413 (683 () ()) (355 (913 (985 () ()) ()) ()))";
+	static String Test3 = "(2 (7 (2 () ()) (6 (5 () ()) (11 () ()))) (5 () (9 (4 () ()) ())))";
+	static String Test4 = "(1 (2 (4 (8 () ()) (9 () ())) (5 (10 () ()) (11 () ()))) (3 (6 (12 () ()) (13 () ())) (7 (14 () ()) (15 () ()))))";
+	static String Test5 = "(1 () (2 () (3 () (5 () (8 () (13 () (21 () (34 () ()))))))))";
+
+   // Expected Output:
+	static int[] Expected1 = new int[]	{1};
+	static int[] Expected2 = new int[]	{985};
+	static int[] Expected3 = new int[]	{5, 11, 4};
+	static int[] Expected4 = new int[]  {8, 9, 10, 11, 12, 13, 14, 15};
+	static int[] Expected5 = new int[]  {34};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeBottom underTest = new TreeBottom();
+		runTest(underTest, Test1, Expected1, "Test1");
+		runTest(underTest, Test2, Expected2, "Test2");
+		runTest(underTest, Test3, Expected3, "Test3");
+		runTest(underTest, Test4, Expected4, "Test4");
+		runTest(underTest, Test5, Expected5, "Test5");
+			
+	};
+	
+	public static void runTest(TreeBottom leaf, String test, int[] expected, String testNum) {
+		int[] results = leaf.treeBottom(test);
+		String exp = Arrays.toString(expected); 
+		String res = Arrays.toString(results);
+		if (exp.equals(res)) {
+			System.out.println("Test1: Equal:"+res);
+		} else {
+			System.out.println("Test1: Expected: " + exp);
+			System.out.println("       Results : " + res);
+		}
+		
+	}
+}
+
